02fdc4aff1b318988c56d39ba02ea630473a04c6
ALDBANDA <alfred.banda1986@gmail.com>
2023-03-03 11:55:45 +0000

    TRFM

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Newbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Newbar />
+    </>
+  )
+}
